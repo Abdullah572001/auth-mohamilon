931f55dcfdbe84c411b2515724a7e4522088d218
Abdullah <mda036060@gmail.com>
2026-06-14 17:42:33 +0600

    add some code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
+
+    const handleLogin = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const pass = e.target.password.value;
+        console.log(email, pass)
+    }
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-2xl font-bold">Login now!</h1>
             
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <form onSubmit={handleLogin} className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" className="input" name="email" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </form>
+              <p className="">new to this website? please <Link className="underline" to="/register">Register</Link></p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
+
 
 const Register = () => {
-    return (
-        <div>
-            
+
+    const handleRegister= e => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const pass = e.target.password.value;
+        console.log(email, pass, name)
+    }
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-2xl font-bold">Register now!</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <form onSubmit={handleRegister} className="fieldset">
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  className="input"
+                  name="name"
+                  placeholder="Name"
+                />
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  name="email"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <button className="btn btn-neutral mt-4">Login</button>
+              </form>
+              <p className="">
+                Already have an account? please {" "}
+                <Link className="underline" to="/login">
+                   Login
+                </Link>
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
